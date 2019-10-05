13e696aef410afcdabfc64733d1cbc4ceba30c63
Melissendra <melissendra77@gmail.com>
2019-10-05 17:11:59 +0200

    hide the select theme message and change the color message

--- js/script.js
+++ js/script.js
@@ -2,28 +2,28 @@
 const $name = $('#name');
 const $otherTitle = $('#other-title');
 const $jobCheck = $("#title");
-const $designItem = $('#design option');
+const $design = $("#design");
+const $designOption = $("#design option");
+const $colors = $("#color");
 const $colorOption = $("#color option");
-const $colorTshirt = $("<option>Please select a T-Shirt theme</option>");
 
 const loadPage = () => {
     $name.trigger('focus'); // I use the trigger() method because it is said that focus() is deprecated and that it was better to use the trigger();
     $otherTitle.hide();
-    $("#color").prepend($colorTshirt);
-    $colorOption.eq(0).prop("selected", true);
+    $design.prepend("<option>-------</option>");
+    $("#design option:first-child").prop("selected", true);
+    $colors.prepend("<option>Please select a T-shirt theme</option>");
+    $("#color option:first-child").prop("selected", true);
 };
 
 
 // we loop through the option of the dropdown
 $jobCheck.each(function(){
     $(this).on("change", function () {
-        if(this.value === 'other'){
-            $otherTitle.show();
-        }else{
-            $otherTitle.hide();
-        }
+        this.value === 'other' ? $otherTitle.show() : $otherTitle.hide();
     });
 });
+
 
 
 loadPage();
